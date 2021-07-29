507d8aa6132f69c0fa7fca770af4b56bc32df89f
itsmushi <jmushi175@gmail.com>
2021-07-29 13:39:47 +0300

    refactor: centering content

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -1,8 +1,71 @@
+import { CircularLoader } from '@dhis2/ui'
+import { useDataQuery } from '@dhis2/app-runtime'
+
+function Introduction(props){
+
+   const indId=props.idIndic
+
+  
+
+    const query = {
+  
+        indicatorsDetails:{
+          resource:"indicators",
+          id:indId,
+          params:{
+            fields:["id","name","displayDescription","numeratorDescription","denominatorDescription",
+            "indicatorType[displayName,id]"
+            ]
+          }
+        }
+      
+      }
+
+     
+    const {loading, error, data}   = useDataQuery(query)
+
+    if(loading){
+        return <CircularLoader />
+     }
+ 
+ 
+     if(error){
+        return <p> {error} </p> 
+     }  
+ 
+     const indicatorDetails=data.indicatorsDetails;
+     console.log(indicatorDetails)  //having trouble getting indicator description
+      
+    return ( <div>
+      
+        <h3>{indicatorDetails.name} </h3>
+
+        <h2>Introduction</h2>
+
+        <p>
+        <b>{indicatorDetails.name} </b> 
+         is a 
+         <b> {indicatorDetails.indicatorType.displayName} </b> 
+          indicator, measured by 
+        <b> {indicatorDetails.numeratorDescription} </b>
+         to  <b> {indicatorDetails.denominatorDescription} </b>
+         </p>
 
 
-function Introduction(){
+        <p>
 
-    
+Its described as {indicatorDetails.displayDescription}
+        </p>
+
+        <p>
+             <span><i> Identified by: {indicatorDetails.id} </i></span>
+        </p>
+                
+        
+    </div>
+    )
+
+
     
 }
 
--- src/pages/indicator.js
+++ src/pages/indicator.js
@@ -1,68 +1,15 @@
 import { useParams,useHistory } from 'react-router-dom'
-import { CircularLoader } from '@dhis2/ui'
-import { useDataQuery } from '@dhis2/app-runtime'
+
+import Introduction from '../components/introduction'
 
 function IndicatiorPage(){
     const { id } = useParams()
- 
    
-    const query = {
-  
-        indicatorsDetails:{
-          resource:"indicators",
-         id,
-          params:{
-            fields:["id","name","description","numeratorDescription","denominatorDescription",
-            "indicatorType[displayName,id]"
-            ]
-          }
-        }
-      
-      }
+    return (<div>
+       <Introduction idIndic={id} /> 
 
-     
-    const {loading, error, data}   = useDataQuery(query)
+       </div>)
 
-    
-
-    if(loading){
-        return <CircularLoader />
-     }
- 
- 
-     if(error){
-        return <p> {error} </p> 
-     }  
- 
-     const indicatorDetails=data.indicatorsDetails;
-     console.log(indicatorDetails)  //having trouble getting indicator description
-      
-    return ( <div>
-      
-        <h3>{indicatorDetails.name} </h3>
-
-        <h2>Introduction</h2>
-
-        <p>
-        <b>{indicatorDetails.name} </b> 
-         is a 
-         <b> {indicatorDetails.indicatorType.displayName} </b> 
-          indicator, measured by 
-        <b> {indicatorDetails.numeratorDescription} </b>
-         to 
-        <b> {indicatorDetails.denominatorDescription} </b>
-
-Its described as {indicatorDetails.description}
-        </p>
-
-        <p>
-             <span><i> Identified by: {indicatorDetails.id} </i></span>
-        </p>
-                
-        
-    </div>
-    )
-  
 }
 
 
