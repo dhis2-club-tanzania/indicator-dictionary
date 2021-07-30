d6909fd85f300352fc37dc8b0fea5825b65e81ab
itsmushi <jmushi175@gmail.com>
2021-07-30 15:57:28 +0300

    feat(dataelementsindicator): dataElementsIndicator

---
+++ src/components/programIndicatorIndicator.js
@@ -0,0 +1,7 @@
+
+
+function ProgramIndicatorIndicator(){
+
+}
+
+export default ProgramIndicatorIndicator
--- src/pages/indicator.js
+++ src/pages/indicator.js
@@ -1,5 +1,6 @@
 import { useParams,useHistory } from 'react-router-dom'
 import CalculationDetails from '../components/calculationDetails'
+import DataElementSIndicator from '../components/dataElementsIndicator'
 
 
 
@@ -19,6 +20,9 @@ function IndicatiorPage(){
        <LegendsAnalysis id={id} />
        <p></p> 
        <CalculationDetails />
+       <p></p>
+       <DataElementSIndicator />
+       <p></p>
 
        </div>)
 
