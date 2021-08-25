d67fb46367e42fcd9a51a0ded815b81cf9513442
jamesmushi <jmushi175@gmail.com>
2021-08-25 11:33:10 +0300

    analytics section query

--- src/Module/DataElement/Components/AnalyticsDetails/index.js
+++ src/Module/DataElement/Components/AnalyticsDetails/index.js
@@ -1,6 +1,31 @@
+import PropTypes from "prop-types";
+import {useDataQuery} from "@dhis2/app-runtime";
+import {useEffect} from 'react'
+import { CircularLoader } from '@dhis2/ui'
+
+
+const query = {
+    dataElements:{
+        resource:"dataElements",
+        id: ({id})=>id,
+        params:{
+            fields:["id","displayName","domainType","zeroIsSignificant","categoryCombo[id,displayName,categories[id,displayName,categoryOptions[id,displayName]]]"
+            ]
+        }
+    }
+
+}
+
 
 
 export default function AnalyticsDetails(){
+
+
+    const {loading, error, data,refetch}  = useDataQuery(query, {variables: {id}})
+
+    useEffect(()=>{refetch({id})},[id])
+
+    console.log(data)
 
     return(<div>
         <h3>Analytics Details</h3>
