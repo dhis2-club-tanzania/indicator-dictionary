881c49dc3bf266631ac1fbc07ff0d99e989a54ef
jamesmushi <jmushi175@gmail.com>
2021-08-25 09:33:59 +0300

    facts added

--- src/Module/DataElement/Components/Facts/index.js
+++ src/Module/DataElement/Components/Facts/index.js
@@ -6,6 +6,15 @@ export default function Facts({id}){
     return (
         <div>
             <h3>Data element Facts</h3>
+            <ul>
+                <li></li>
+                {/*<li> Accepts only {{valueType}} to enforce validation</li>*/}
+                {/*<li>Has {{validationRulesMatchCount}} related validation rules</li>*/}
+                {/*<li> Part of numerators of {{indicatorNumeratorExpressionMatchCount}} indicators</li>*/}
+                {/*<li>Part of denominators of {{indicatorDenominatorExpressionMatchCount}} indicators</li>*/}
+
+
+            </ul>
         </div>
     )
 }
