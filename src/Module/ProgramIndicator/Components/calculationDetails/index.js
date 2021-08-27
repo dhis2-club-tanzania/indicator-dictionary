22107e33fd68845281eefbaf3b20ceb44b254472
itsmushi <jmushi175@gmail.com>
2021-08-27 12:28:51 +0300

    Program Indicator Calculation detail query

--- src/Module/ProgramIndicator/Components/calculationDetails/index.js
+++ src/Module/ProgramIndicator/Components/calculationDetails/index.js
@@ -1,9 +1,38 @@
 import ExpressionDetailTable from "./Components/ExpressionDetailTable";
 import BoundaryTable from "./Components/BoundaryTable";
-import React from "react";
+import React, {useEffect} from "react";
+import {useDataQuery} from "@dhis2/app-runtime";
+import Loader from "../../../../Shared/Componets/Loaders/Loader";
+import Error from "../../../../Shared/Componets/Error/ErrorAPIResult";
+
+
+const query =  {
+    calculation:{
+        resource:"programIndicators",
+        id: ({id})=>id,
+        params:{
+            fields:["analyticsType","aggregationType","expression","filter","analyticsPeriodBoundaries[boundaryTarget,analyticsPeriodBoundaryType,offsetPeriods,offsetPeriodType]"]
+        }
+    }
+}
 
 
 export default function CalculationDetails({id}){
+
+
+    const {loading, error, data,refetch}  = useDataQuery(query, {variables: {id}})
+
+    useEffect(()=>{refetch({id})},[id])
+
+
+    if(loading){
+        return  <Loader text={""} />
+    }if(error){
+        return <Error error={error} />
+    }
+
+
+    console.log(data?.calculation)
 
 
     return <div>
@@ -12,18 +41,18 @@ export default function CalculationDetails({id}){
         </p>
         <p>
             Program indicator calculation will be based on {"{analyticsType}"}, for distinction purposes:
-            <ul>
-                <li>
-                    Events implies, each event from data source is considered as independent row to be counted, and properties and details of the event are used to filter events.
 
-                </li>
-                <li>
-                    Enrollment implies, each enrollment from data source is considered as independent row to be counted, and events from any stage and other properties and details of enrollment are used to filter enrollments.
-
-                </li>
-            </ul>
         </p>
+        <ul>
+            <li>
+                Events implies, each event from data source is considered as independent row to be counted, and properties and details of the event are used to filter events.
 
+            </li>
+            <li>
+                Enrollment implies, each enrollment from data source is considered as independent row to be counted, and events from any stage and other properties and details of enrollment are used to filter enrollments.
+
+            </li>
+        </ul>
         <div>
             <p>
 
