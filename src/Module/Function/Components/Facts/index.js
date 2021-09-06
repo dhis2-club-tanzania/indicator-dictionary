3173b8372523589d445615c4bb4d58bee7d08658
itsmushi <jmushi175@gmail.com>
2021-09-06 15:53:57 +0300

    facts works

--- src/Module/Function/Components/Facts/index.js
+++ src/Module/Function/Components/Facts/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import {formatBytes} from "../../../../Utils/Functions/FormulaFunctions";
+const { Buffer } = require('buffer');
 
 export default function Facts({selected}){
-
 
 
     return <div>
         <h3>Function Facts</h3>
 
-        <u>
-            <li>   It is approximately  {"{function character length, see this for more details https://mothereff.in/byte-counter}"} bytes/kilobytes in size</li>
+        <ul>
+            <li> It is approximately {formatBytes(Buffer.byteLength(selected?.function,'utf-8'),2) } in size</li>
             <li>It has {selected?.rules?.length} associated rules</li>
             <li> {selected?.functions?.search('Fn')>0?' It’s using function analytics library' :' It’s not using function analytics library'}</li>
-            <li>  {selected?.functions?.search('$.')>0?'It’s using jquery api library':'It’s not using jquery api library'}</li>
+            <li>  {selected?.functions?.search('$.')>0?'It’s using jqusome stringery api library':'It’s not using jquery api library'}</li>
             <li> {selected?.functions?.search('$.ajax')>0? 'Performs ajax promises ':"Does not performs ajax promises"}</li>
             <li>{selected?.functions?.search('../../../api/')>0?'Fetches from DHIS2 API without function analytics':'Does not fetches from DHIS2 API without function analytics'} </li>
             <li>Running on API version: {"{dhis2 version from system info }"}</li>
-        </u>
+        </ul>
 
         </div>
 }
--- src/Module/Function/Components/Introduction/index.js
+++ src/Module/Function/Components/Introduction/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import i18n from '@dhis2/d2-i18n'
 import IdentifiedBy from "../../../../Shared/Componets/IdentifiedBy/Index";
 
 
@@ -7,8 +8,8 @@ export default function Introduction({selected}){
 
 
     return <div>
-       <h3>Introduction</h3>
-       <p> <b> {selected?.displayName} </b> can be best described as:
+       <h3>{i18n.t("Introduction")} </h3>
+       <p> <b> {selected?.displayName} </b> {i18n.t(" can be best described as:")}
         {selected?.description}. </p>
         <IdentifiedBy id={selected?.id} href={selected?.href}  />
 
